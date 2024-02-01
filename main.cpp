fba54809475e338dc01fc665ad70b8ac7a3e60c0
MaximKanevskiy <merser.zombie@gmail.com>
2024-02-01 22:35:10 +0300

    v1.2

--- main.cpp
+++ main.cpp
@@ -4,40 +4,24 @@
 
 std::vector<std::string> towerBuilder(unsigned nFloors) 
 {
+	std::vector<std::string> pyramid(nFloors);
 	char block = '*';
 	int count = 1;
 	int space = nFloors - 1;
-	std::vector<std::string> pyramid;
 
 	for (int i = 0; i < nFloors; i++)
 	{
-		std::string floor = "";
-
-		for (int k = 0; k < space; k++)
-		{
-			floor += " ";
-		}
-
-		for (int j = 0; j < count; j++)
-		{
-			floor += block;
-		}
-
-		for (int k = 0; k < space; k++)
-		{
-			floor += " ";
-		}
-		
+		pyramid[i] = std::string(space, ' ') + std::string(count, '*') + std::string(space, ' ');
 		count += 2;
-		space--;
-		pyramid.push_back(floor);
+		space--;		
 	}
+
 	return pyramid;
 }
 
 int main()
 {
-	auto pyramid = towerBuilder(3);
+	auto pyramid = towerBuilder(10);
 	for (const auto& floor : pyramid)
 	{
 		std::cout << floor << std::endl;
