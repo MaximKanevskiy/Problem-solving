ca4b4ba7af833ea5997d76f80bacd2765056ece0
MaximKanevskiy <merser.zombie@gmail.com>
2024-02-03 14:52:34 +0300

    Added description

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// Структура узла.
+// Имеет ячейку для информации и ячейку для указателя на следующий узел.
 class Node
 {
 public:
@@ -7,6 +9,7 @@ public:
 	Node* next;
 };
 
+// Печатает односвязный список.
 static void print_linked_list(Node* node)
 {
     while (node != nullptr)
@@ -16,6 +19,9 @@ static void print_linked_list(Node* node)
     }
 }
 
+// Добавляет узел в начало списка.
+// Новый узел будет указывать на старый начальный,
+// а ссылка на начало списка переместится на новый узел.
 static void push(Node*& head_reference, int new_data)
 {
     Node* new_node = new Node();
@@ -24,6 +30,10 @@ static void push(Node*& head_reference, int new_data)
     head_reference = new_node;
 }
 
+// Добавляет в определённое место списка.
+// Принимает ссылку на предыдущий узел и информацию для нового узла.
+// Новый узел будет указывать на тот, на который раньше указывал предыдущий,
+// а предыдущий будет указывать на новый.
 static void insert(Node*& previous_node, int new_data) 
 {
     if (previous_node == nullptr) 
@@ -38,6 +48,9 @@ static void insert(Node*& previous_node, int new_data)
     previous_node->next = new_node;
 }
 
+// Добавляет узел в конец списка. 
+// Принимает на вход начальный узел и новую информацию для нового узла.
+// Новый узел, так как он в конце, никуда не указывает.
 static void append(Node*& head_reference, int new_data) 
 {
     Node* new_node = new Node();
@@ -53,6 +66,25 @@ static void append(Node*& head_reference, int new_data)
     last->next = new_node;
 }
 
+// Удаляет односвязный список. Принимает на вход начало списка.
+static void delete_linked_list(Node*& head_reference)
+{
+    Node* current_node = head_reference;
+    Node* next = nullptr;
+
+    while (current_node != nullptr)
+    {
+        next = current_node->next;
+        delete current_node;
+        current_node = next;
+    }
+
+    head_reference = nullptr;
+}
+
+// Все узлы явно инициализируются nullptr и создаются динамически с помощью оператора new.
+// Затем все узлы должны быть заполнены определёнными данными, например, целыми числами.
+// Все кроме последнего указывают на следующий узел. Последний никуда не указывает.
 int main()
 {
     Node* head = nullptr;
@@ -78,4 +110,6 @@ int main()
     append(head, 78);
     std::cout << std::endl;
     print_linked_list(head);
+
+    delete_linked_list(head);
 }
