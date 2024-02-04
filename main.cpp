078895826fa81dd738b66a4054e405f5ad91d5fa
MaximKanevskiy <merser.zombie@gmail.com>
2024-02-04 11:18:38 +0300

    Can you get the loop?

--- main.cpp
+++ main.cpp
@@ -1,115 +1,71 @@
 #include <iostream>
 
-// Структура узла.
-// Имеет ячейку для информации и ячейку для указателя на следующий узел.
 class Node
 {
-public:
-	int data;
+private:
 	Node* next;
+public:
+	Node()
+	{
+		this->next = nullptr;
+	}
+
+	void setNext(Node* next)
+	{
+		this->next = next;
+	}
+
+	Node* getNext()
+	{
+		return this->next;
+	}
 };
 
-// Печатает односвязный список.
-static void print_linked_list(Node* node)
+static int getLoopSize(Node* startNode)
 {
-    while (node != nullptr)
-    {
-        std::cout << node->data << " ";
-        node = node->next;
-    }
+	if (startNode == nullptr)
+	{
+		return 0;
+	}
+
+	Node* turtleptr = startNode;
+	Node* rabbitptr = startNode;
+
+	while (rabbitptr != nullptr && rabbitptr->getNext() != nullptr)
+	{
+		turtleptr = turtleptr->getNext();
+		rabbitptr = rabbitptr->getNext()->getNext();
+
+		if (turtleptr == rabbitptr)
+		{
+			int loopSize = 1;
+			Node* current = turtleptr;
+
+			while (current->getNext() != turtleptr)
+			{
+				loopSize++;
+				current = current->getNext();
+			}
+
+			return loopSize;
+		}
+	}
+
+	return 0;
 }
 
-// Добавляет узел в начало списка.
-// Новый узел будет указывать на старый начальный,
-// а ссылка на начало списка переместится на новый узел.
-static void push(Node*& head_reference, int new_data)
-{
-    Node* new_node = new Node();
-    new_node->data = new_data;
-    new_node->next = head_reference;
-    head_reference = new_node;
-}
-
-// Добавляет в определённое место списка.
-// Принимает ссылку на предыдущий узел и информацию для нового узла.
-// Новый узел будет указывать на тот, на который раньше указывал предыдущий,
-// а предыдущий будет указывать на новый.
-static void insert(Node*& previous_node, int new_data) 
-{
-    if (previous_node == nullptr) 
-    {
-        std::cout << "the given previous node cannot be NULL" << std::endl;
-        return;
-    }
-
-    Node* new_node = new Node();
-    new_node->data = new_data;
-    new_node->next = previous_node->next;
-    previous_node->next = new_node;
-}
-
-// Добавляет узел в конец списка. 
-// Принимает на вход начальный узел и новую информацию для нового узла.
-// Новый узел, так как он в конце, никуда не указывает.
-static void append(Node*& head_reference, int new_data) 
-{
-    Node* new_node = new Node();
-    Node* last = head_reference;
-    new_node->data = new_data;
-    new_node->next = nullptr;
-
-    while (last->next != nullptr)
-    {
-        last = last->next;
-    }
-
-    last->next = new_node;
-}
-
-// Удаляет односвязный список. Принимает на вход начало списка.
-static void delete_linked_list(Node*& head_reference)
-{
-    Node* current_node = head_reference;
-    Node* next = nullptr;
-
-    while (current_node != nullptr)
-    {
-        next = current_node->next;
-        delete current_node;
-        current_node = next;
-    }
-
-    head_reference = nullptr;
-}
-
-// Все узлы явно инициализируются nullptr и создаются динамически с помощью оператора new.
-// Затем все узлы должны быть заполнены определёнными данными, например, целыми числами.
-// Все кроме последнего указывают на следующий узел. Последний никуда не указывает.
 int main()
 {
-    Node* head = nullptr;
-    Node* second = nullptr;
-    Node* third = nullptr;
+	Node n0, n00, n1, n2, n3, n4;
 
-    head = new Node();
-    second = new Node();
-    third = new Node();
+	n0.setNext(&n00);
+	n00.setNext(&n1);
+	n1.setNext(&n2);
+	n2.setNext(&n3);
+	n3.setNext(&n4);
+	n4.setNext(&n1);
 
-    head->data = 1;
-    head->next = second;
-
-    second->data = 2;
-    second->next = third;
-
-    third->data = 3;
-    third->next = nullptr;
-
-    print_linked_list(head);
-    push(head, 56);
-    insert(second, 100);
-    append(head, 78);
-    std::cout << std::endl;
-    print_linked_list(head);
-
-    delete_linked_list(head);
+	Node* startNode = &n1;
+	int actual = getLoopSize(startNode);
+	std::cout << actual << std::endl;
 }
