3c4afb4883e9c68883831dbdec02c4b75be79c73
MaximKanevskiy <merser.zombie@gmail.com>
2024-02-05 12:43:36 +0300

    Cursed answer

--- main.cpp
+++ main.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
 #include <vector>
-
-static bool isValidWalk(std::vector<char> walk) 
-{
-	if (walk.size() != 10)
-	{
-		return false;
-	}
-
-	int vertical_balance = 0;
-	int horizontal_balance = 0;
-
-	for (const auto& direction : walk)
-	{
-		switch (direction)
-		{
-		case 'n':
-			vertical_balance++;
-			break;
-		case 's':
-			vertical_balance--;
-			break;
-		case 'w':
-			horizontal_balance++;
-			break;
-		case 'e':
-			horizontal_balance--;
-			break;
-		}
-	}
-
-	return vertical_balance == 0 && horizontal_balance == 0;
-}
-
-int main()
-{
-	std::vector correct_walk = { 'e','e','e','w','n','s','n','s','e','w' };
-	std::cout << isValidWalk(correct_walk) << std::endl;
+#include <map>
+#include <stack>
+class DirReduction {
+public:
+    static bool is_opposite(const std::string& direction1, const std::string& direction2) {
+        std::map<std::string, std::string> opposite = { {"NORTH", "SOUTH"}, {"SOUTH", "NORTH"}, {"EAST", "WEST"}, {"WEST", "EAST"} };
+        return opposite[direction1] == direction2;
+    }
+    static std::vector<std::string> dirReduc(std::vector<std::string>& arr) {
+        std::stack<std::string> current_direction;
+        for (size_t i = 0; i < arr.size(); i++) {
+            if (current_direction.empty()) {
+                current_direction.push(arr[i]);
+                continue;
+            }
+            if (is_opposite(arr[i], current_direction.top())) current_direction.pop();
+            else current_direction.push(arr[i]);
+        }
+        std::vector<std::string> reduced_direction;
+        while (!current_direction.empty()) {
+            reduced_direction.push_back(current_direction.top());
+            current_direction.pop();
+        }
+        std::reverse(reduced_direction.begin(), reduced_direction.end());
+        return reduced_direction;
+    }
+};
+int main() {
+    DirReduction reducer;
+    std::vector<std::string> given_direction = { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "NORTH" };
+    auto result = reducer.dirReduc(given_direction);
+    for (const auto& direction : result) std::cout << direction << " ";
 }
